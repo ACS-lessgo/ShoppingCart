67b544483e6628a27d7ed270b4e05c7b1f8e0575
ACS-lessgo <royalcs379@gmail.com>
2024-12-04 18:27:16 +0530

    [BUG] fixes table name

--- src/main/java/com/acs/Shopping/Cart/model/Order.java
+++ src/main/java/com/acs/Shopping/Cart/model/Order.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,6 +27,7 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "orders")
 public class Order {
 
     @Id
